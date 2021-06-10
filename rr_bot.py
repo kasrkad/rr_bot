10feab35cd6fa617c57b4d2c6327132bbf2e9ee8
i.meleshenkov <i.meleshenkov@infinnity.ru>
2021-06-10 15:52:11 +0500

    try get http page

--- rr_bot.py
+++ rr_bot.py
@@ -1,1 +1,34 @@
 #!/bin/python3
+import requests
+from requests.models import HTTPBasicAuth
+from requests.sessions import session
+from bs4 import BeautifulSoup
+from bot_config import AUTH
+import time
+import bot_config
+
+rr_page = 'https://hpsm.emias.mos.ru/sm/index.do?lang='
+session  = requests.Session()
+
+authorization = session.post('https://hpsm.emias.mos.ru/sm/index.do?lang=', {
+	'user.id': 'MeleshenkovI',
+	'xHtoken': None,
+	'old.password': 'Boradori4040',
+	'L.language': 'ru',
+	'type':'login',
+	'originalUrl':'https://hpsm.emias.mos.ru/sm/index.do?lang=ru',
+	} )
+
+
+rr = session.get(rr_page, headers=authorization.headers)
+
+print (rr.text)
+
+
+# rr = session.get(rr_page)
+# print(session.cookies)
+
+# rr = session.get(rr_page, auth= HTTPBasicAuth('MeleshenkovI', 'Boradori4040'))
+# print(rr)
+# print (rr.text)
+# html = BeautifulSoup(rr.text, features='html.parser')
