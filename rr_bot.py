f829621dd1c570b324fba75b56b3cdde99a767f6
kasrkad <kasrkin90@gmail.com>
2021-06-11 09:13:00 +0500

    authorization probe

--- rr_bot.py
+++ rr_bot.py
@@ -3,24 +3,38 @@ import requests
 from requests.models import HTTPBasicAuth
 from requests.sessions import session
 from bs4 import BeautifulSoup
-from bot_config import AUTH
+# from bot_config import AUTH
 import time
-import bot_config
+# import bot_config
 
-rr_page = 'https://hpsm.emias.mos.ru/sm/index.do?lang='
+rr_page = 'https://hpsm.emias.mos.ru/sm/index.do?lang=ru'
 session  = requests.Session()
 
-authorization = session.post('https://hpsm.emias.mos.ru/sm/index.do?lang=', {
+authorization = session.post('https://hpsm.emias.mos.ru/sm/index.do?lang=', data={
 	'user.id': 'MeleshenkovI',
 	'xHtoken': None,
 	'old.password': 'Boradori4040',
-	'L.language': 'ru',
-	'type':'login',
+	 'L.language': 'ru',
+	 'type':'login',
 	'originalUrl':'https://hpsm.emias.mos.ru/sm/index.do?lang=ru',
 	} )
+print(authorization)
+some_headers = dict(authorization.headers)
+one, two, three, four, five, six = some_headers['set-cookie'].split(";")
 
+print (one[11:])
 
-rr = session.get(rr_page, headers=authorization.headers)
+head = {  'Cookie': f'JSESSIONID={one[11:]}; mode=index.do; lang=ru',
+    'lang':'ru',
+    'mode':"index.do",
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Host':'hpsm.emias.mos.ru',
+    'Connection':'keep-alive',
+    'Referer': 'https://hpsm.emias.mos.ru/sm/index.do'
+}
+print(head)
+
+rr = session.get(rr_page, headers = head )
 
 print (rr.text)
 
