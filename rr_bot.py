20e16e5bc1beb13db37416451cae3fc953e50395
i.meleshenkov <i.meleshenkov@infinnity.ru>
2021-06-16 08:09:42 +0500

    working parse iframe

--- rr_bot.py
+++ rr_bot.py
@@ -1,48 +1,23 @@
 #!/bin/python3
 import requests
-from requests.models import HTTPBasicAuth
-from requests.sessions import session
+from selenium import webdriver
 from bs4 import BeautifulSoup
-# from bot_config import AUTH
 import time
+# from bot_config import AUTH
 # import bot_config
+url = 'https://hpsm.emias.mos.ru/sm/index.do'
+driver = webdriver.Chrome('/mnt/c/Tools/rr_bot/chromedriver.exe')
+driver.get(url)
+driver.find_element_by_id("LoginUsername").send_keys("MeleshenkovI")
+driver.find_element_by_id("LoginPassword").send_keys("Boradori4040")
+driver.find_element_by_id("loginBtn").click()
+time.sleep(5)
+driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
 
-rr_page = 'https://hpsm.emias.mos.ru/sm/index.do?lang=ru'
-session  = requests.Session()
-
-authorization = session.post('https://hpsm.emias.mos.ru/sm/index.do?lang=', data={
-	'user.id': 'MeleshenkovI',
-	'xHtoken': None,
-	'old.password': 'Boradori4040',
-	 'L.language': 'ru',
-	 'type':'login',
-	'originalUrl':'https://hpsm.emias.mos.ru/sm/index.do?lang=ru',
-	} )
-print(authorization)
-some_headers = dict(authorization.headers)
-one, two, three, four, five, six = some_headers['set-cookie'].split(";")
-
-print (one[11:])
-
-head = {  'Cookie': f'JSESSIONID={one[11:]}; mode=index.do; lang=ru',
-    'lang':'ru',
-    'mode':"index.do",
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
-    'Host':'hpsm.emias.mos.ru',
-    'Connection':'keep-alive',
-    'Referer': 'https://hpsm.emias.mos.ru/sm/index.do'
-}
-print(head)
-
-rr = session.get(rr_page, headers = head )
-
-print (rr.text)
+with open ('html', 'w') as f:
+   f.write(driver.page_source)
 
 
-# rr = session.get(rr_page)
-# print(session.cookies)
 
-# rr = session.get(rr_page, auth= HTTPBasicAuth('MeleshenkovI', 'Boradori4040'))
-# print(rr)
-# print (rr.text)
-# html = BeautifulSoup(rr.text, features='html.parser')
+
+# result = driver.execute_script("https://hpsm.emias.mos.ru/sm/js/9.63.0006/sm.recordlist.js")
