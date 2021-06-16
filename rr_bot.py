b44410aaf5f23862767bbcc2c8d28068f8455e2c
i.meleshenkov <i.meleshenkov@infinnity.ru>
2021-06-16 17:36:46 +0500

    add logout from HPSM

--- rr_bot.py
+++ rr_bot.py
@@ -10,20 +10,33 @@ def Get_HPSM_table(url):
    if os.path.exists('html'):
       os.remove('html')
    options = webdriver.ChromeOptions()
-   options.add_argument('--headless')
-   driver = webdriver.Chrome(config.PATH_TO_DRIVER, options=options)
+   # options.add_argument('--headless')
+   driver = webdriver.Chrome(config.PATH_TO_DRIVER)
    driver.get(url)
    driver.find_element_by_id("LoginUsername").send_keys(config.USER_NAME)
    driver.find_element_by_id("LoginPassword").send_keys(config.PASSWORD)
    driver.find_element_by_id("loginBtn").click()
    time.sleep(5)
-   driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
-
-   with open ('html', 'w') as f:
-      f.write(driver.page_source)
+   while True:
+      if os.path.exists('html'):
+         os.remove('html')
+      driver.get(url)
       
-   driver.Quit()
+      time.sleep(5)
+      driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
+      time.sleep(2)
+      with open ('html', 'w') as f:
+         f.write(driver.page_source)
+      driver.switch_to.default_content()
+      time.sleep(5)
+      driver.get('https://hpsm.emias.mos.ru/sm/goodbye.jsp?lang=')
+      time.sleep(3)
+      parse_hpsm('html')
+      driver.get(url)
+      driver.find_element_by_id("LoginUsername").send_keys(config.USER_NAME)
+      driver.find_element_by_id("LoginPassword").send_keys(config.PASSWORD)
+      driver.find_element_by_id("loginBtn").click()
+
 
 if __name__ == '__main__':
    Get_HPSM_table(config.url)
-   parse_hpsm('html')
