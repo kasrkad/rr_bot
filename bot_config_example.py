24c60328640ce41b8747e5900f288463c09ae10d
i.meleshenkov <i.meleshenkov@infinnity.ru>
2021-06-22 09:32:46 +0500

    add chat_id parameter

--- bot_config_example.py
+++ bot_config_example.py
@@ -1,8 +1,9 @@
 #!/bin/python3
 
-url = 'https://hpsm.emias.mos.ru/sm/index.do'
+url = 'URL_FOR_PARSE'
 USER_NAME = 'USERNAME'
 PASSWORD = 'PASSWORD'
 GECKOPASS = r'/path/to/geckodriver'
 BOT_TOKEN = 'TOKEN'
 CHECK_TIME = 300 #secs
+CHAT_ID = 'chatID'
