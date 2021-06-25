9eaef850ed53f145ff0e3e61c1e481824ee10e80
i.meleshenkov <i.meleshenkov@infinnity.ru>
2021-06-25 14:34:42 +0500

    working download on dev from commit

--- bot.py
+++ bot.py
@@ -1,14 +1,66 @@
 #!/bin/python3
 import os
+import subprocess
 import telebot
 import config
 import json
-from bot_functions import check_duty_eng, get_document
+from bot_functions import check_duty_eng, get_document, remove_files
+import glob
 
 rr_bot = telebot.TeleBot(config.BOT_TOKEN, parse_mode='MARKDOWN')
 
 global duty_eng
 duty_eng = check_duty_eng()
+
+@rr_bot.message_handler(commands=['в_приоритете'])
+def artifact_download_dev(message): # грузит только на дев и только с коммита
+    msg = rr_bot.reply_to(message,"введите ссt")
+    rr_bot.register_next_step_handler(msg,cct_enter_step)
+
+def cct_enter_step(message):
+    try:
+        cct = message.text
+        with open('./simi_cli/ids', 'w', encoding='utf8') as enter_cct_file:
+           enter_cct_file.write(f"{cct}; ")
+        print (cct)
+        msg = rr_bot.reply_to(message,"введите параметры загрузки артефактов в формате simi-cli : cct, vis, tmpl.\n Пример: cct, vis ")
+        rr_bot.register_next_step_handler(msg,artifact_enter_step)
+    except Exception as exc:
+        rr_bot.reply_to(message,f"Что то пошло не так,{exc}")
+
+def artifact_enter_step(message):
+    artifacts = message.text
+    print(artifacts)
+    with open('./simi_cli/ids', 'a', encoding='utf8') as enter_artifacts_file:
+        enter_artifacts_file.write(artifacts)
+    msg = rr_bot.reply_to(message,"Введите коммит: \nПример: 81dsf93")
+    rr_bot.register_next_step_handler(msg,simi_cli_check_and_start)
+
+def simi_cli_check_and_start(message):
+    global commit
+    commit = message.text
+    print(commit)
+    with open ('./simi_cli/ids','r', encoding='utf8') as check_ids_file:
+        simi_cli_ids = check_ids_file.readline()
+    print (f"Проверьте настройки для simi_cli\n ids {simi_cli_ids},\n commit {commit}")
+    msg = rr_bot.reply_to(message,f"Проверьте настройки для simicli,\nДля загрузки: {simi_cli_ids},\nCommit: {commit}\nЕсли все корректно, введите Y")
+    rr_bot.register_next_step_handler(msg,start_cli)
+
+def start_cli(message):
+    answer = message.text
+    if answer.strip() == 'Y':
+        rr_bot.send_message(message.chat.id,f"Стартуем simicli с параметрами: applycommit dev {commit}")
+        # out = subprocess.call(['./simi_cli/artifact.sh','applycommit','dev', commit])
+        out = subprocess.Popen(['./simi_cli/artifact.sh','applycommit','dev', commit], stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+        out.wait()
+        logs_for_send = glob.glob('./logs/*log.txt')
+        rr_bot.send_message(message.chat.id,"Лови логи")
+        for files in logs_for_send:
+            doc = open(files)
+            rr_bot.send_document(message.from_user.id,doc)
+        remove_files(glob.glob("./logs/*"))
+    else:
+        rr_bot.send_message(message.chat.id,"Запуск отменен, переделывай.")
 
 @rr_bot.message_handler(commands=['скачай'])
 def download_document(message):
