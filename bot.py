fe7f639c35f27cb88f078997598f77dbb472ef48
kasrkad <kasrkin90@gmail.com>
2021-06-27 23:27:28 +0500

    add exception and validating functions in bot_functions

--- bot.py
+++ bot.py
@@ -4,47 +4,54 @@ import subprocess
 import telebot
 import config
 import json
-from bot_functions import check_duty_eng, get_document, remove_files
+import bot_functions 
 import glob
 
 rr_bot = telebot.TeleBot(config.BOT_TOKEN, parse_mode='MARKDOWN')
 
 global duty_eng
-duty_eng = check_duty_eng()
+duty_eng = bot_functions.check_duty_eng()
 
-@rr_bot.message_handler(commands=['в_приоритете'])
+@rr_bot.message_handler(commands=['cct'])
 def artifact_download_dev(message): # грузит только на дев и только с коммита
     msg = rr_bot.reply_to(message,"введите ссt")
-    rr_bot.register_next_step_handler(msg,cct_enter_step)
+    rr_bot.register_next_step_handler(msg ,cct_enter_step)
 
 def cct_enter_step(message):
     try:
-        cct = message.text
+        cct = bot_functions.validate_cct_string(message)
         with open('./simi_cli/ids', 'w', encoding='utf8') as enter_cct_file:
-           enter_cct_file.write(f"{cct}; ")
+            enter_cct_file.write(f"{cct}; ")
         print (cct)
-        msg = rr_bot.reply_to(message,"введите параметры загрузки артефактов в формате simi-cli : cct, vis, tmpl.\n Пример: cct, vis ")
+        msg = rr_bot.reply_to(message,"Введите параметры загрузки артефактов в формате simicli : cct, vis, tmpl.\n Пример: cct, vis ")
         rr_bot.register_next_step_handler(msg,artifact_enter_step)
-    except Exception as exc:
-        rr_bot.reply_to(message,f"Что то пошло не так,{exc}")
+    except ValueError as exc:
+        rr_bot.send_message(exc.args[1],exc.args[0])
 
 def artifact_enter_step(message):
-    artifacts = message.text
-    print(artifacts)
-    with open('./simi_cli/ids', 'a', encoding='utf8') as enter_artifacts_file:
-        enter_artifacts_file.write(artifacts)
-    msg = rr_bot.reply_to(message,"Введите коммит: \nПример: 81dsf93")
-    rr_bot.register_next_step_handler(msg,simi_cli_check_and_start)
+    try:
+        artifacts = bot_functions.validate_artifact_string(message) 
+        print(artifacts)
+        with open('./simi_cli/ids', 'a', encoding='utf8') as enter_artifacts_file:
+            enter_artifacts_file.write(artifacts)
+        msg = rr_bot.reply_to(message,"Введите коммит: \nПример: 81dsf93")
+        rr_bot.register_next_step_handler(msg,simi_cli_check_and_start)
+    except ValueError as exc:
+        rr_bot.send_message(exc.args[1],exc.args[0])
 
 def simi_cli_check_and_start(message):
     global commit
-    commit = message.text
-    print(commit)
-    with open ('./simi_cli/ids','r', encoding='utf8') as check_ids_file:
-        simi_cli_ids = check_ids_file.readline()
-    print (f"Проверьте настройки для simi_cli\n ids {simi_cli_ids},\n commit {commit}")
-    msg = rr_bot.reply_to(message,f"Проверьте настройки для simicli,\nДля загрузки: {simi_cli_ids},\nCommit: {commit}\nЕсли все корректно, введите Y")
-    rr_bot.register_next_step_handler(msg,start_cli)
+    try:
+        commit = bot_functions.validate_commit_string(message)
+        print(commit)
+        with open ('./simi_cli/ids','r', encoding='utf8') as check_ids_file:
+            simi_cli_ids = check_ids_file.readline()
+        print (f"Проверьте настройки для simi_cli\n ids {simi_cli_ids},\n commit {commit}")
+        msg = rr_bot.reply_to(message,f"Проверьте настройки для simicli,\nДля загрузки: {simi_cli_ids},\nCommit: {commit}\nЕсли все корректно, введите Y")
+        rr_bot.register_next_step_handler(msg,start_cli)
+    except ValueError as exc:
+        print(exc.args)
+        rr_bot.send_message(exc.args[1], exc.args[0])
 
 def start_cli(message):
     answer = message.text
@@ -94,9 +101,4 @@ def who_duty_today(message):
         rr_bot.reply_to(message,f"Дежурный не обнаружен, вызываем ответственного: [Александр Бауман](tg://user?id=753785354)")
 
 if __name__=='__main__':
-
-	try:
-		rr_bot.polling(none_stop=True, interval=0, timeout=20)
-	except Exception as exc:
-		with open ('bot_exc.txt', 'a', encoding='utf8') as exc_file:
-			exc_file.write(str(exc) + "\n")
+    rr_bot.polling(none_stop=True, interval=0, timeout=30)
--- bot_functions.py
+++ bot_functions.py
@@ -2,6 +2,8 @@
 import os
 import json
 import requests
+import re
+from config import CORRECT_ARTIFACTS
 
 def remove_files (list):
     for file in list:
@@ -14,3 +16,25 @@ def check_duty_eng():
         return duty_eng
     else:
         return None
+
+def validate_cct_string(cct_message):
+    cct = cct_message.text
+    if len(cct) == 5:
+        if cct.isdigit():
+            return cct
+    raise ValueError(f'Не корректный номер ССТ: {cct_message.text}, ССТ = 5 цифр',cct_message.chat.id)
+
+def validate_artifact_string(artifact_message):
+    artifacts = artifact_message.text
+    artifacts_for_check = artifacts.split(',')
+    artifacts_for_check = set(artifacts_for_check)
+    for artifact in artifacts_for_check:
+        if artifact.strip().lower() not in CORRECT_ARTIFACTS:
+            raise ValueError(f'Нeкорректные данные в списке артефактов {artifact_message.text}', artifact_message.chat.id)
+    return f'{",".join(x.strip().lower() for x in artifacts_for_check)}'
+
+def validate_commit_string(commit_message):
+    commit = commit_message.text
+    if re.match(r'[a-z0-9]', commit.lower()):
+        return commit.lower()
+    raise ValueError(f"Некорректный вид коммита {commit_message.text}, должно быть 9 символов, английские буквы и цифры, отсутствовать спецзнаки", commit_message.chat.id)   
