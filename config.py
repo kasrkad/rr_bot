f4885b5053d36e02025782b7fad5859212b61bbb
Ivan Meleshenkov <kasrkin90@gmail.com>
2022-10-26 16:44:03 +0500

    [update]: Дописал модуль запросов к СИМИ, перенес запросы из конфига в модуль запросов.

--- config.py
+++ config.py
@@ -7,19 +7,7 @@ PRODUCER_SOAP_PASS = os.getenv('PRODUCER_SOAP_PASS')
 SIMI_DNS_NAME_ENDPOINT_TEMPLATE = os.getenv('SIMI_DNS_NAME_ENDPOINT_TEMPLATE')
 SIMIP3_DNS_NAME_ENDPOINT_TEMPLATE = os.getenv('SIMIP3_DNS_NAME_ENDPOINT_TEMPLATE')
 
-PRODUCER_REQUEST = '''<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ru="ru.emias.simi.v2.api.diagnostic.v1">
-      <soapenv:Header>
-      <wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
-         <wsse:UsernameToken wsu:Id="UsernameToken-50">
-            <wsse:Username>{soap_name}</wsse:Username>
-            <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">{soap_pass}</wsse:Password>
-         </wsse:UsernameToken>
-      </wsse:Security>
-   </soapenv:Header>
-   <soapenv:Body>
-      <ru:recreateEsuProducer/>
-   </soapenv:Body>
-</soapenv:Envelope>'''
+
 
 #Настройки доступа до Репозитория ССТ
 SIMI_DOC_REPO_FOLDER = os.getenv('SIMI_DOC_REPO_FOLDER')
@@ -91,54 +79,6 @@ SIMI_IP_73 = os.getenv('SIMI_IP_73')
 CORRECT_ARTIFACTS = ['cct','vis','tmpl']
 DUTY_OWNER = '[Александр Бауман](tg://user?id=753785354)'
 
-#Для скачивания документов
-
-REQUEST_BODY = """<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:user="http://emias.mos.ru/system/v1/userContext/" xmlns:typ="http://emias.mos.ru/simi/simiService/v5/types/" xmlns:v5="http://emias.mos.ru/simi/document/v5/" xmlns:v51="http://emias.mos.ru/simi/core/v5/">
-   <soap:Header>
-    <wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
-        <wsse:UsernameToken wsu:Id="UsernameToken-50">
-            <wsse:Username>{PPAK_SOAP_USER}</wsse:Username>           
-        </wsse:UsernameToken>
-    </wsse:Security>
-    <user:userContext>
-        <user:systemName>SIMI</user:systemName>
-        <user:userName>simi_support/tg:{tg}</user:userName>
-        <user:userRoleId>1</user:userRoleId>
-    </user:userContext>
-</soap:Header>
-   <soap:Body>
-      <typ:getDocumentRequest>
-         <v5:documentId>{id}</v5:documentId>
-         <!--Optional:-->
-            </typ:getDocumentRequest>
-   </soap:Body>
-</soap:Envelope>"""
-
-ASTERISK_PAYLOAD = {
-"display": "ringgroups",
-"action": "edtGRP",
-"account": "627",
-"description": "EMIAS-duty",
-"strategy": "ringall",
-"grptime": "20",
-"annmsg_id": None,
-"ringing": "Ring",
-"grppre": None,
-"alertinfo": None,
-"remotealert_id": None,
-"toolate_id": None,
-"changecid": "default",
-"recording": "dontcare",
-"goto0": "Ring_Groups",
-"Ring_Groups0": "ext-group,628,1",
-"Submit": "Submit Changes"
-}
-
-ASTERISK_GROUP = 'https://pbx/config.php?display=ringgroups&extdisplay=GRP-627'
-
-ASTERSK_HEADERS = {'Referer':'https://pbx/config.php?display=ringgroups&extdisplay=GRP-627'}
-
-ASTERISK_LOGIN = {"input_user": ASTERISK_LOGIN, "input_pass": ASTERISK_PASS, "submit_login": "Submit"}
 STAND_75 = {
     "75":[
     f'--document-registry-url jdbc:oracle:thin:@{ORACLE_75}',
--- simi_requests_module/request_to_simi.py
+++ simi_requests_module/request_to_simi.py
@@ -2,33 +2,124 @@ import requests
 import os
 import logging
 
+#configure logger
 request_to_simi_logger = logging.getLogger('request_to_simi_logger')
 request_to_simi_logger_formatter = logging.Formatter(
     "%(name)s %(asctime)s %(levelname)s %(message)s")
 request_to_simi_logger.setLevel(logging.INFO)
-
 request_to_simi_logger_handler_file = logging.FileHandler("simi_request.log", 'a')
 request_to_simi_logger_handler_file.setLevel(logging.INFO)
 request_to_simi_logger_handler_file.setFormatter(request_to_simi_logger_formatter)
 request_to_simi_logger.addHandler(request_to_simi_logger_handler_file)
 
 
-
-
 PRODUCER_SOAP_PASS = os.environ['PRODUCER_SOAP_PASS']
 PRODUCER_SOAP_USER = os.environ['PRODUCER_SOAP_USER']
 SIMI_DNS_NAME_ENDPOINT_TEMPLATE = os.environ['SIMI_DNS_NAME_ENDPOINT_TEMPLATE']
-HEADERS = headers = {'content-type': 'text/xml'}
+SIMIP3_DNS_NAME_ENDPOINT_TEMPLATE = os.environ['SIMIP3_DNS_NAME_ENDPOINT_TEMPLATE']
+HEADERS = {'content-type': 'text/xml'}
 
-def set_diagnostic_endpoint_request(request_to_set, state = False):
-    try:
-        request_body = request_to_set.format(state=state)
-        request_to_simi_logger.info(f'Отправлен запрос на изменение {request_to_set} параметра на {state}')
-        for node_num in range(1,13):
-            url = SIMI_DNS_NAME_ENDPOINT_TEMPLATE.format(i=node_num)
-            # response = requests.post(url, data=request_body, headers=HEADERS)
-        request_to_simi_logger.info(f"Параметр {state} установлен")
-        raise Exception("Очеень страшний ошибпка!11")
-    except Exception as exc:
-        request_to_simi_logger.error(f"Произошла ошибка при установлении параметра {request_to_simi_logger}", exc_info=True)
-        raise
+
+def set_request_diagnostic_endpoint(request_to_set, state = False) -> dict:
+    request_body = request_to_set.format(state=state)
+    request_to_simi_logger.info(f'Отправлен запрос на изменение {request_to_set} параметра на {state}')
+    request_nodes_result = {}
+    
+    for node_num_simi in range(1,13):
+        url = SIMI_DNS_NAME_ENDPOINT_TEMPLATE.format(i=node_num_simi)
+        try:
+            response = requests.post(url, data=request_body, headers=HEADERS)
+            request_nodes_result[url] = response.status_code
+        except Exception as exc:
+            request_nodes_result[url] = "error, see logs"
+            request_to_simi_logger.error(f"Произошла ошибка при установлении параметра {state} на узел {url}", exc_info=True)
+    
+    for node_num_simi_asinc in range(1,3):
+        url = SIMIP3_DNS_NAME_ENDPOINT_TEMPLATE.format(i=node_num_simi_asinc)
+        try:
+            response = requests.post(url, data=request_body, headers=HEADERS)
+            request_nodes_result[url] = response.status_code
+        except Exception as exc:
+            request_nodes_result[url] = "error, see logs"
+            request_to_simi_logger.error(f"Произошла ошибка при установлении параметра {state} на узел {url}", exc_info=True)
+    
+    return request_nodes_result
+
+
+def get_request_diagnostic_endpoint(request_to_get) -> dict:
+    request_to_simi_logger.info(f'Отправлен запрос на проверку состояния {request_to_get}')
+    request_nodes_result = {}
+    
+    for node_num_simi in range(1,13):
+        url = SIMI_DNS_NAME_ENDPOINT_TEMPLATE.format(i=node_num_simi)
+        try:
+            response = requests.post(request_to_get, data=request_body, headers=HEADERS)
+            if "true" in response.text:
+                request_nodes_result[url] = True
+            else:
+                request_nodes_result[url] = False
+        except Exception as exc:
+            request_nodes_result[url] = "error, see logs"
+    
+    for node_num_simi_asinc in range(1,3):
+        url = SIMIP3_DNS_NAME_ENDPOINT_TEMPLATE.format(i=node_num_simi_asinc)
+        try:
+            response = requests.post(request_to_get, data=request_body, headers=HEADERS)
+            if "true" in response.text:
+                request_nodes_result[url] = True
+            else:
+                request_nodes_result[url] = False
+        except Exception as exc:
+            request_nodes_result[url] = "error, see logs"
+    
+    return request_nodes_result
+
+
+def producer_recreate_request(producer_recreate_request) -> dict:
+    request_body = producer_recreate_request.format(soap_name=PRODUCER_SOAP_USER,
+                                                    soap_pass=PRODUCER_SOAP_PASS)
+    request_to_simi_logger.info(f'Отправлен запрос на пересоздание продьюсера.')
+    request_nodes_result = {}
+    
+    for node_num_simi in range(1,13):
+        url = SIMI_DNS_NAME_ENDPOINT_TEMPLATE.format(i=node_num_simi)
+        try:
+            response = requests.post(url, data=request_body, headers=HEADERS)
+            request_nodes_result[url] = response.status_code
+        except Exception as exc:
+            request_nodes_result[url] = "error, see logs"
+            request_to_simi_logger.error(f"Произошла ошибка при запросе на пересоздание продьюсера на узле {url}", exc_info=True)
+    
+    for node_num_simi_asinc in range(1,3):
+        url = SIMIP3_DNS_NAME_ENDPOINT_TEMPLATE.format(i=node_num_simi_asinc)
+        try:
+            response = requests.post(url, data=request_body, headers=HEADERS)
+            request_nodes_result[url] = response.status_code
+        except Exception as exc:
+            request_nodes_result[url] = "error, see logs"
+            request_to_simi_logger.error(f"Произошла ошибка при запросе на пересоздание продьюсера на узле {url}", exc_info=True)
+    
+    return request_nodes_result
+
+
+def simi_document_request(request_to_simi, stand_node_adress, documents_ids=list, requester_tg_id=None) -> list:
+    if not os.path.exists("./docs/"):
+        os.mkdir("./docs/")
+
+    request_to_simi_logger.info(f'Пользователем {requester_tg_id} запрошены документы {",".join(doc for doc in documents_ids)}.')
+    path_way_for_files = []
+    for doc_id in documents_ids:
+        try:
+            request_body = request_to_simi.format(id=doc_id.strip(),
+                                                tg=requester_tg_id,
+                                                PPAK_SOAP_USER=PPAK_SOAP_USER)
+            query_result = requests.post(stand_node_adress,
+                                        data = request_body,
+                                        headers =HEADERS)
+            with open("./docs/documents_output/" + doc_id + ".xml", "wb+") as id_file:
+                id_file.write(response.text.encode('utf-8').strip())
+            path_way_for_files.append(f'./docs/{doc_id}.xml')
+        except Exception as exc:
+            request_to_simi_logger.error(f"Произошла ошибка при работе с документами", exc_info=True)
+    
+    return path_way_for_files
