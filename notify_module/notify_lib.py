07d1ad3511ec9111c32c8f45b5a90166d77c92dd
Ivan Meleshenkov <kasrkin90@gmail.com>
2022-11-23 20:06:08 +0500

    [update]:Доделал модуль уведомлений,добавлена загрузка в базу из файла,обработки исключений.

--- notify_module/notify_lib.py
+++ notify_module/notify_lib.py
@@ -1,6 +1,7 @@
 import sys
+import logging
 sys.path.append('../')
-from sqlite_module.sql_lib import get_all_notifys,create_tables,change_notify_status,midnight_reset_notifications
+from sqlite_module.sql_lib import get_all_notifys,create_tables,change_notify_status,midnight_reset_notifications,insert_nofity
 
 #configure logger
 notify_logger = logging.getLogger('notify_logger')
@@ -10,7 +11,7 @@ notify_logger.setLevel(logging.INFO)
 notify_logger_logger_handler_file = logging.FileHandler("notify.log", 'a')
 notify_logger_logger_handler_file.setLevel(logging.INFO)
 notify_logger_logger_handler_file.setFormatter(notify_logger_formatter)
-notify_logger.addHandler(request_to_simi_logger_handler_file)
+notify_logger.addHandler(notify_logger_logger_handler_file)
 
 
 class Notify:
@@ -27,8 +28,9 @@ class Notify:
 
 
 class Notifyer:
-    def __init__(self):
+    def __init__(self,path_to_file_with_notifycations= None):
         self.notifycations = []
+        self.path_to_file_with_notifycations = path_to_file_with_notifycations
 
     def check_for_notify(self, obj):
         
@@ -52,20 +54,38 @@ class Notifyer:
         else :
             print("Не о чем уведомлять")
 
-    def load_all_notyfications_db(self):
+    def load_all_notyfications_from_db(self):
         notify_logger.info('Загружаем все уведомления из базы данных.')
         try:
             all_notifys = get_all_notifys()
             keys = ("bd_name" , "time", "work_day", "text", "status", "target")
             for notify_data in all_notifys:
                 self.notifycations.append(Notify(**dict(zip(keys,notify_data))))
+            print(len(self.notifycations))
         except Exception as exc:
             notify_logger.error('При загрузке уведомлений произошла ошибка', exc_info=True)
+
+    def load_standard_notify_from_file(self):
+        from json import load
+        try:
+            if self.path_to_file_with_notifycations:
+                notify_logger.info(f'Загружаем базовые уведомления из файла {self.path_to_file_with_notifycations}')
+                with open(self.path_to_file_with_notifycations, 'r', encoding='utf8') as notify_file:
+                    data_for_db_insert = load(notify_file)
+                    for notify in data_for_db_insert['standard_notify']:
+                        insert_nofity(**notify)
+                notify_logger.info('Базовые уведомления успешно загружены.')
+                return
+            notify_logger.info(f'Не задан файл со стандартными уведомлениями, нечего загружать')
+        except Exception as exc:
+            notify_logger.error('Ошибка загрузки стандартных уведомлений', exc_info=True)
+        
 
     def run(self):
         notify_logger.info('Notifyer запущен.')
         from time import sleep
-        self.load_all_notyfications_db()
+        self.load_standard_notify_from_file()
+        self.load_all_notyfications_from_db()
         try:
             while True:
                 for _n in self.notifycations:
@@ -74,3 +94,4 @@ class Notifyer:
                 sleep(20)
         except Exception as exc:
             notify_logger.error('В процессе работы Notifyer произошла ошибка', exc_info=True)
+            
