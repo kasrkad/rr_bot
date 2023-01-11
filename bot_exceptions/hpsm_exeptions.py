fc9219d0bb5e7890c7625a9eafdfe8e479e27ccd
i.meleshenkov <i.meleshenkov@infinnity.ru>
2023-01-11 13:14:07 +0500

    [update]: Добавил исключения для парсера HPSM

---
+++ bot_exceptions/hpsm_exeptions.py
@@ -0,0 +1,45 @@
+class GetHpsmFrameException(Exception):
+
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return f'GetHpsmFrameException, {self.message}'
+        else:
+
+            return 'GetHpsmFrameException has been raised'
+
+
+class GetPageException(Exception):
+
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return f'GetPageException, {self.message}'
+        else:
+
+            return 'GetPageExeption has been raised'
+
+
+class HpsmLoginException(Exception):
+
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return f'HpsmLoginException, {self.message}'
+        else:
+            return 'HpsmLoginException has been raised'
