ff93d97a7217b800ea2ec0c2871e24a619d39736
kasrkad <kasrkin90@gmail.com>
2023-02-05 22:08:15 +0500

    [fix]:удалил лишние файлы

--- hpsm_bot.py
+++
@@ -1,6 +0,0 @@
-from os import environ
-from ess_bot.hpsm_module.hpsm_checker import Hpsm_checker
-TG_BOT_TOKEN = environ["TG_BOT_TOKEN"]
-
-checker = Hpsm_checker(bot_token=TG_BOT_TOKEN,rr_file_path='./rr_list.txt',request_codes_file_path='./request_codes.txt')
-checker.start()
